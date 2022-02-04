a0ec4818c5135db4437ffbec73717452c53418a2
Manuel Covas <manuel.p.covas@gmail.com>
2022-02-04 01:53:35 +0000

    Fixed permissions with copied file.

--- extend.sh
+++ extend.sh
@@ -1,3 +1,3 @@
-su -c "cp /data/user/0/nextapp.fx/shared_prefs/nextapp.fx_preferences.xml . && chown everybody:everybody nextapp.fx_preferences.xml" && \
+su -c "cp /data/user/0/nextapp.fx/shared_prefs/nextapp.fx_preferences.xml . && chmod 666 nextapp.fx_preferences.xml" && \
 python extend.py && \
 su -c "cp nextapp.fx_preferences.xml /data/user/0/nextapp.fx/shared_prefs"
